e0460b358d7ebd76b9c73dc0580b9dbd370916d1
Alexandre pontes silva junior <alexandre.pontes.jr@gmail.com>
2024-05-15 21:36:39 -0400

    retirada de lixo

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,8 +1,5 @@
-import binance from "./binance.png";
-import coinbase from "./coinbase.png";
-import dropbox from "./dropbox.png";
+
 import logo from "./logo.png";
-import quotes from "./quotes.svg";
 import send from "./Send.svg";
 import shield from "./Shield.svg";
 import star from "./Star.svg";
@@ -11,14 +8,6 @@ import close from "./close.svg";
 import google from "./google.svg";
 import pix from "./pix.svg";
 import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
-import facebook from "./facebook.svg";
-import instagram from "./instagram.svg";
-import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
 import logoPoe from "./logoPoe.png";
 import search from "./search.svg";
 import whatsapp from "./whatsapp.svg";
@@ -28,18 +17,8 @@ import diabloIcon from "./diabloIcon.png";
 import lastEpocIcon from "./lastEpocIcon.png";
 import poeIcon from "./poeIcon.png";
 import carrinho from "./carrinho.svg";
-import dealItems from "./dealItems.png";
-import itemsHero from "./itemsHero.webp";
-import servicesImage from "./servicesImage.webp";
 import ShopItemCoin from "./ShopItemCoin.png";
-import shop_item_bg from "./shop_item_bg.png";
-import arrowNotch from "./arrowNotch.webp";
-import bullet from "./bullet.webp";
 import mercadoIcone from "./mercado-icone.svg";
-import CurrencyItems from "./MuitosCurrencysItems.svg";
-import espada from "./espada.svg";
-import bgCurrency from "./bg-currency.svg";
-import whatsappBt from "./whatsapp_bt.png";
 import currencys from "./currencys.png";
 import logoNovo from "./logo.png";
 import d4Banner from "./d4Banner.jpg";
@@ -50,20 +29,10 @@ export {
   currencys,
   d4Preto,
   logoNovo,
-  arrowNotch,
-  whatsappBt,
-  bgCurrency,
-  espada,
-  CurrencyItems,
   mercadoIcone,
   payPal,
-  bullet,
   carrinho,
-  shop_item_bg,
   ShopItemCoin,
-  servicesImage,
-  itemsHero,
-  dealItems,
   logoPoe,
   lastEpocIcon,
   poeIcon,
@@ -71,11 +40,7 @@ export {
   moneyBag,
   whatsapp,
   search,
-  binance,
-  coinbase,
-  dropbox,
   logo,
-  quotes,
   send,
   shield,
   star,
@@ -83,13 +48,5 @@ export {
   close,
   google,
   pix,
-  arrowUp,
-  discount,
-  facebook,
-  instagram,
-  linkedin,
-  twitter,
-  people01,
-  people02,
-  people03
+  arrowUp
 };
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,4 +1,3 @@
-import { people01, people02, people03, send, shield, star  } from "../assets";
 import { GameDetails, getGameDetailsById } from "./games.js"
 
 export {
@@ -30,83 +29,11 @@ export const navLinks = [
 ];
 
 export const features = [
-  {
-    id: "feature-1",
-    icon: star,
-    title: "Rewards",
-    content:
-      "The best credit cards offer some tantalizing combinations of promotions and prizes",
-  },
-  {
-    id: "feature-2",
-    icon: shield,
-    title: "100% Secured",
-    content:
-      "We take proactive steps make sure your information and transactions are secure.",
-  },
-  {
-    id: "feature-3",
-    icon: send,
-    title: "Balance Transfer",
-    content:
-      "A balance transfer credit card can save you a lot of money in interest charges.",
-  },
+
 ];
 
 export const feedback = [
-  {
-    id: "1",
-    content: "Estou muito satisfeito com o serviço ! Excelente experiência!",
-    name: "Adriana Silva",
-    title: "Engenheira",
-    img: people01,
-    star: 5,
-  },
-  {
-    id: "2",
-    content:
-      "O serviço superou todas as minhas expectativas! Recomendo totalmente.",
-    name: "João Silva",
-    title: "Advogado",
-    img: people02,
-    star: 4,
-  },
-  {
-    id: "3",
-    content:
-      "Nunca pensei que jogos online poderiam ser tão envolventes! Estou impressionado com o serviço.",
-    name: "Mariana Santos",
-    title: "Designer",
-    img: people02,
-    star: 5,
-  },
-  {
-    id: "4",
-    content:
-      "Simplesmente incrível! O serviço me proporcionou horas de diversão sem fim.",
-    name: "Gabriel Lima",
-    title: "Founder & Leader",
-    img: people03,
-    star: 5,
-  },
-  {
-    id: "5",
-    content:
-      "Estou completamente viciado nos serviços oferecidos! Melhor investimento que fiz.",
-    name: "Rafael Junior",
-    title: "Founder & Leader",
-    img: people02,
-    star: 5,
-  },
-  {
-    id: "6",
-    content:
-      "Estou completamente viciado nos serviços oferecidos! Melhor investimento que fiz.",
-    name: "Pedro Teixeira",
-    title: "Founder & Leader",
-    img: people01,
-    star: 5,
-  },
+
 ];
 
 export const stats = [
