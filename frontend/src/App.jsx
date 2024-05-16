77aa6227c00148c0e76fc5fd99a119d9a2de4a85
Alexandre pontes silva junior <alexandre.pontes.jr@gmail.com>
2024-05-15 22:10:18 -0400

    sombreamento

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import {
   ScrollTop,
   Billing,
@@ -7,9 +7,9 @@ import {
   Services,
   Testimonials,
   Footer,
-  FAQ,
-} from "./components";
-import styles from "./styles";
+  FAQ
+} from './components';
+import styles from './styles';
 
 const App = ({ game }) => {
   const [loaded, setLoaded] = useState(false);
@@ -19,47 +19,52 @@ const App = ({ game }) => {
   };
 
   useEffect(() => {
-    document.title = "RealmXcoins - " + game.name;
+    document.title = 'RealmXcoins - ' + game.name;
   });
 
   return (
     <>
       <div className="bg-black w-full">
-        <div className="backgroundFundoPreto">
+        <div className="video-container">
           <div className={` ${styles.flexCenter} `}>
             <video
-              className={`hidden md:block video ${loaded ? "loaded" : ""} `}
+              className={`hidden md:block video ${loaded ? 'loaded' : ''}`}
               autoPlay
               muted
               loop
-              onLoadedData={handleVideoLoaded}
-            >
+              onLoadedData={handleVideoLoaded}>
               <source src={game.videoSrc} type="" />
             </video>
-
-            <div className={`${styles.boxWidth2}`}>
-              <Navbar game={game} />
-              <Hero game={game} />
+            <div className="sombreamento"></div>
+            <div className="">
+              <div className={` ${styles.boxWidth2}`}>
+                <Navbar game={game} />
+                <Hero game={game} />
+              </div>
             </div>
           </div>
         </div>
         <ScrollTop game={game} />
         <div className={`${styles.flexStart}`}>
+
           <div className={`${styles.boxWidth2} `}>
             <Billing game={game} />
           </div>
         </div>
         <div className={`${game.cssClassBG} ${styles.flexStart}`}>
+        <div className="sombreamento"></div>
           <div className={`${styles.boxWidth2}`}>
             <Services game={game} />
           </div>
         </div>
+
         <div className={` ${styles.flexStart}`}>
           <div className={`${styles.boxWidth2}`}>
             <Testimonials game={game} />
           </div>
         </div>
         <div className={`${game.cssClassBgFaq} ${styles.flexStart}`}>
+        <div className="sombreamento"></div>
           <div className={`${styles.boxWidth2}`}>
             <FAQ game={game} />
           </div>
